531a655235cd617b4e694c129fa097fb6856303e
kelta-king <kushangshah131@gmail.com>
2024-01-03 22:34:31 +0530

    no server needed

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 
 const app = express(); 
 const PORT = 3000; 
-const hostname = "0.0.0.0";
 
 app.get('/', (req, res)=>{ 
     res.status(200); 
     res.send("Welcome to root URL of Server"); 
 }); 
 
-app.listen(PORT, hostname, (error) =>{ 
+app.listen(PORT, (error) =>{ 
 	if(!error) 
 		console.log("Server is Successfully Running, and App is listening on port "+ PORT) 
 	else
