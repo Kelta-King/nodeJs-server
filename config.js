920c1a29c504ae9a3b0b456c6d1e7d3140b2a4dd
kelta-king <kushangshah131@gmail.com>
2024-09-05 23:28:36 +0530

    3D model server added

--- config.js
+++ config.js
@@ -1,5 +1,22 @@
+const os = require('os');
+
+function getIP() {
+    const networkInterfaces = os.networkInterfaces();
+    var IP = "127.0.0.1";
+    Object.keys(networkInterfaces).forEach((interfaceName) => {
+        networkInterfaces[interfaceName].forEach((iface) => {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                console.log(iface.address);
+                IP = iface.address;
+            }
+        });
+    });
+    return IP;
+}
+
 const CONFIG = {
-    PORT: 3001
+    PORT: 3001,
+    ADDRESS: getIP(),
 }
 
 module.exports = CONFIG;
--- main.js
+++ main.js
@@ -5,10 +5,41 @@ const CONFIG = require('./config');
 
 const app = express();
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+function loadModelsObject() {
+
+    const modelsDirectory = path.join(__dirname, 'public', 'Models');
+    
+    const folders = fs.readdirSync(modelsDirectory).filter(file => {
+        const fullPath = path.join(modelsDirectory, file);
+        return fs.statSync(fullPath).isDirectory();
+    });
+    
+    var models = [];
+    
+    folders.forEach(folder => {
+        const modelsDir = path.join(modelsDirectory, folder, 'info.json');
+        const data = JSON.parse(fs.readFileSync(modelsDir));
+        const commonURL = "http://" + CONFIG.ADDRESS + ":" + CONFIG.PORT + "/Models/" + folder + "/";
+        models.push({
+            name: data.name,
+            description: data.description,
+            src: {
+                model: commonURL + data.model,
+                preview: commonURL + data.preview,
+            }
+        });
+    });
+    
+    console.log('Loaded models:', models);
+    return models;
+}
+
+const globalModels = loadModelsObject();
 
 app.get('/api/getModels', (req, res) => {
-    
+    res.json(globalModels);
 });
 
 app.listen(CONFIG.PORT, () => {
